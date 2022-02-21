a810829783e94def8463ab281cdd15909cb10cd7
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-02-21 15:11:02 -0800

    all columns showing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import data from './Data';
+import MaterialTable from 'material-table';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <p>hello!</p>
+      <MaterialTable
+        options={{
+          search: false
+        }}
+        title='people'
+        columns={[
+          { title: 'first name' },
+          { title: 'last name' },
+          { title: 'gender' },
+          { title: 'age' },
+          { title: 'pet' },
+          { title: 'favorite food' },
+        ]}
+        data={data}/>
     </div>
   );
 }
