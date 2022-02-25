5186b619c4adbd94e98aa477eff79a98b456e893
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-02-25 11:11:04 -0800

    color bars function working!

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ function App() {
         />
         <VictoryBar 
           alignment='start'
-          style={{ data: { fill: ({ datum }) => datum.x === 'Green' ? 'green' : datum.x === 'Pink' ? 'pink' : datum.x === 'Yellow' ? 'yellow' : datum.x === 'Puce' ? '#CC8899' : datum.x === 'Turquoise' ? 'turquoise' : datum.x === 'Violet' ? 'violet' : datum.x === 'Goldenrod' ? 'goldenrod' : datum.x === 'Blue' ? 'blue' : datum.x === 'Purple' ? 'purple' : datum.x === 'Khaki' ? 'khaki' : datum.x === 'Fuscia' ? '#D9027D' : datum.x === 'Aquamarine' ? 'aquamarine' : datum.x === 'Maroon' ? 'maroon' : datum.x === 'Mauv' ? '#BB85AB' : datum.x === 'Indigo' ? 'indigo' : datum.x === 'Orange' ? 'orange' : datum.x === 'Teal' ? 'teal' : datum.x === 'Crimson' ? 'crimson' : datum.x === 'Red' ? 'red' : '', width: 10 } }}
+          style={{ data: { fill: ({ datum }) => colorBars(datum), width: 10 } }}
           data={faveColor(data)}
       
         />
