85475e6eaeeb3a07e19f46cf0b45a11daaad5fac
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-02-25 12:00:26 -0800

    updated test

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,14 @@
+/* eslint-disable no-unused-vars */
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import data from './data';
+import { VictoryBar, 
+  VictoryChart, 
+  VictoryAxis, 
+  VictoryLabel, 
+  VictoryPortal, 
+  VictoryPie,
+  VictoryLine } from 'victory';
 
 test('renders learn react link', () => {
   render(<App />);
