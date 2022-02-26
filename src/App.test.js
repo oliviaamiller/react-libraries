9566ea31a084b3b94988fa21ae58c523961e5829
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-02-25 17:28:25 -0800

    imports for test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,21 @@
 /* eslint-disable no-unused-vars */
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import data from './data';
+import './App.css';
+import { VictoryBar, 
+  VictoryChart, 
+  VictoryAxis, 
+  VictoryLabel, 
+  VictoryPortal, 
+  VictoryPie,
+  VictoryLine } from 'victory';
+import MaterialTable from 'material-table';
+import { carsInCountry, faveColor, agesOverTime } from './data-utils';
+import { colorBars } from './functions';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/My Charts /i);
+  const linkElement = screen.getByText(/My Charts/i);
   expect(linkElement).toBeInTheDocument();
 });
